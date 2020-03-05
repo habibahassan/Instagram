53c8eccdcde2fdf441a97a9f9317f409feb777b5
habibahassan <halimaadan92@gmail.com>
2020-03-05 23:02:31 +0300

    added view function for loginrequired on views.py

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def instagram(request):
+    posts = Image.get_images()
+    return render(request, "instagram.html", {"posts":posts})
