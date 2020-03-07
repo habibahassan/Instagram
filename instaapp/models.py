98bf578fb577092adf2010c9e04142b69dbc81a4
habibahassan <halimaadan92@gmail.com>
2020-03-07 21:49:45 +0300

    created image model on models.py

--- instaapp/models.py
+++ instaapp/models.py
@@ -18,3 +18,34 @@ class Profile(models.Model):
     def search_profile(cls,search_term):
         profiles = cls.objects.filter(user__icontains=search_term)
         return profiles
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'photos/', default='DEFAULT VALUE')
+    owner = models.ForeignKey(User, null=True, on_delete=models.CASCADE,related_name="user_name")
+    profile = models.ForeignKey(Profile,null=True)
+    image_name = models.CharField(max_length =30)
+    caption = models.CharField(max_length =50)
+    post_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+    
+
+    @classmethod
+    def get_all_images(cls):
+        all_images = Image.objects.all()
+        for image in all_images:
+            return image
+
+    @classmethod
+    def update_image(cls,current,new):
+        to_update = Image.objects.filter(image_name=current).update(image_name=new)
+        return to_update
+    @classmethod
+    def get_image_by_id(cls,id):
+        image_result = cls.objects.get(id=id)
+        return image_result
