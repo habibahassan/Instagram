c6a818b745de0fd2dcdff8e202cb32205b80e772
habibahassan <halimaadan92@gmail.com>
2020-03-07 21:40:10 +0300

    created view fuction for homepage

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
 
 # Create your views here.
+def homePageView(request):
+    return HttpResponse('Hello, World!')
 
 @login_required(login_url='/accounts/login/')
 def instagram(request):
     posts = Image.get_images()
-    return render(request, "instagram.html", {"posts":posts})
+    return render(request, "", {"posts":posts})
