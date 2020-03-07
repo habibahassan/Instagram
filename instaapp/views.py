deae08cbf7b43fefa0320f9dd5c84136e3411cb4
habibahassan <halimaadan92@gmail.com>
2020-03-07 22:15:07 +0300

    changed my view function

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from .models import Image
+from django.contrib.auth.models import User
 
 
 # Create your views here.
-def homePageView(request):
-    return HttpResponse('Hello, World!')
-
 @login_required(login_url='/accounts/login/')
-def instagram(request):
-    posts = Image.get_images()
-    return render(request, "", {"posts":posts})
+def home(request):
+    images = Image.objects.all().order_by('-post_date')
+    users = User.objects.all()  
+    current = request.user
+    
+    return render(request, 'index.html',{"images":images,'users':users,'current':current,})
