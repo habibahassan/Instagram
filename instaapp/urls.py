ebf99b53f9c14e8b516ba7c11cc8a27f8cb9307e
habibahassan <halimaadan92@gmail.com>
2020-03-09 08:27:49 +0300

    added url paths to url.py

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('profile/',views.profile,name='profile'),
+    path('profile/update/',views.update_profile,name='update_profile'),
+    path('no profile/',views.no_profile),
+    path('search/', views.search_results, name='search_results'),
+    path('new/image/',views.new_image,name="new_image"),
+    path('comment/', views.comment, name='comment'),
+    path('comment/like/',views.like_pic,name="like"),
+    path('image/update',views.update_image,name='update_image'),
+    path('profile/follow/', views.follow, name='follow'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
